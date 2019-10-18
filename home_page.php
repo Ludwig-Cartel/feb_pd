1d605233509bd5939f6f0849ad22e2bb992f40da
Ludwig Cartel <cartel.ludwig@gmail.com>
2019-10-18 13:50:35 +0800

    vanilla

---
+++ home_page.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
---
+++ resources/php/class/home_page_script.php
@@ -0,0 +1,3 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/feb_pd/resources/php/db/config.php';
+?>
